a81846d037ea41c34cc2c4f3a8c6c639e0ab2472
Jamie Adams <jamie.adams@liveoak360.com>
2012-03-18 15:56:01 -0500

    first commit - basic rails scaffolding, working from http://delicious.com/redirect?url=http%3A//pragmaticstudio.com/blog/2010/9/23/install-rails-ruby-mac setup tutorial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'b0235482abab7e3eb7c91451ed59b648bb121a671f4d33e210d4986411b875332f0f4a67fdc543db872392ad81997915e834557cebfaa19847360006382f12c4'
